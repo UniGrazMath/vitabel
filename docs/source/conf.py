f4d528dab197d9bcd01f74137402a92765d96809
Benjamin Hackl <devel@benjamin-hackl.at>
2025-01-25 02:25:01 +0100

    ruff

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'vitabel'
-copyright = '2025, Benjamin Hackl, Wolfgang Kern, Simon Orlob'
-author = 'Benjamin Hackl, Wolfgang Kern, Simon Orlob'
+project = "vitabel"
+copyright = "2025, Benjamin Hackl, Wolfgang Kern, Simon Orlob"
+author = "Benjamin Hackl, Wolfgang Kern, Simon Orlob"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -22,16 +22,20 @@ numpydoc_show_class_members = False
 autoapi_dirs = ["../../src"]
 autoapi_template_dir = "_autoapi_templates"
 autoapi_options = [
-    'members', 'undoc-members', 'private-members', 'show-inheritance', 'show-module-summary', 'special-members'
+    "members",
+    "undoc-members",
+    "private-members",
+    "show-inheritance",
+    "show-module-summary",
+    "special-members",
 ]
 
-templates_path = ['_templates', '_autoapi_templates']
+templates_path = ["_templates", "_autoapi_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
