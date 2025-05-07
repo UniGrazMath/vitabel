b9c6b6cbb708522e167de734f4939cfa487db42b
Simon Orlob <simon.orlob@medunigraz.at>
2025-05-08 01:59:45 +0200

    corrected the datatypes

--- src/vitabel/utils/type_defs.py
+++ src/vitabel/utils/type_defs.py
@@ -9,3 +9,5 @@ class Metric:
 class ThresholdMetrics:
     area_under_threshold: Metric
     minutes_under_threshold: Metric
+    time_weighted_average_under_threshold: Metric
+    minutes_observational_interval: Metric
