95f274ada59241bab24acb447a557b21fe16b30a
Simon Orlob <simon.orlob@medunigraz.at>
2025-05-23 09:07:33 +0200

    feat: transforming csv into dataframes

--- src/vitabel/utils/loading.py
+++ src/vitabel/utils/loading.py
@@ -2882,3 +2882,119 @@ def read_corpuls(f_corpuls):  # Read Corpuls Data
         **twelve_lead_ecg_dict,
     }
     return pat_dat, data
+
+
+def read_eolife_export(f_eolife: Path):
+    # EOlife export data are written in csv format
+    # the first three line contain header information on the recording
+    # line 4 contains the heder for the recording data
+    # the rest of the file contains the data
+
+    header_info = pd.read_csv(
+        f_eolife,
+        sep=";",
+        header=0,
+        nrows=1,
+        encoding="latin1",
+        na_values="NA",
+        usecols=[
+            "Date",
+            "Time",
+            "Patient Type",
+            "Patient Size",
+            "Mode",
+            "Training",
+            "FrequencyMode",
+            "Leakage alarm",
+            "EOlife"
+        ],
+        dtype=str,
+    )
+
+    header_info["recording_start"] = pd.to_datetime(
+        header_info["Date"] + " " + header_info["Time"],
+        format="%d/%m/%y %H:%M:%S"
+    )
+
+    header_info.drop(columns=["Date", "Time"],inplace=True)
+    header_info.rename(
+        columns={
+            "EOlife": "serial_number",
+        },
+        inplace=True
+    )
+
+    recording_start = header_info["recording_start"].iloc[0]
+    metadata = header_info[["Patient Type",
+            "Patient Size",
+            "Mode",
+            "Training",
+            "FrequencyMode",
+            "Leakage alarm",]].iloc[0].to_dict()
+    
+    df_data = pd.read_csv(
+        f_eolife, 
+        sep=";", 
+        decimal=",", 
+        skiprows=3, 
+        header=0,
+        encoding="latin1",
+        na_values="NA",
+        usecols=[
+            "Cycle number",
+            "Time (hh:mm:ss:SS)",
+            "Ti (ms)",
+            "Te (ms)",
+            "Tp (ms)",
+            "Freq (/min)",
+            "Vi (mL)",
+            "Vt (mL)",
+            "Leakage (mL)",
+            "Leakage ratio (%)"
+        ] ,
+        dtype={
+            "Cycle number": int,
+            "Time (hh:mm:ss:SS)": str,
+            "Ti (ms)": "Int64",
+            "Te (ms)": "Int64",
+            "Tp (ms)": "Int64",
+            "Freq (/min)": "Int64",
+            "Vi (mL)": "Int64",
+            "Vt (mL)": "Int64",
+            "Leakage (mL)": "Int64",
+        },
+        converters={
+            "Leakage ratio (%)": lambda x: float(x) / 100 if x != "NA" else None
+        }
+    )
+
+    df_data.rename(columns={"Leakage ratio (%)": "Leakage ratio"}, inplace=True)
+
+    df_data["timedelta"] = pd.to_timedelta(
+        df_data["Time (hh:mm:ss:SS)"].str.replace(
+            r"(\d{2}):(\d{2}):(\d{2}):(\d{2})", r"\1:\2:\3.\4", regex=True
+        )
+    )
+    df_data.set_index("timedelta", inplace=True)
+    df_data.drop(columns=["Time (hh:mm:ss:SS)"], inplace=True)
+
+    # Step 1: Extract units and create a rename + units mapping
+    unit_pattern = re.compile(r"^(.*?)\s*\((.*?)\)$")
+
+    rename_dict = {}
+    units_dict = {}
+
+    for col in df_data.columns:
+        if "(" in col and ")" in col:
+            name_part = col[:col.find("(")].strip()
+            unit_part = col[col.find("(")+1 : col.find(")")].strip()
+            rename_dict[col] = name_part
+            units_dict[name_part] = unit_part
+
+    # Step 2: Rename columns
+    df_data.rename(columns=rename_dict, inplace=True)
+
+
+
+
+
