34d82ee168e0b7dc79ad7ad60d548a3573ae8d0c
Simon Orlob <60964035+sorlob@users.noreply.github.com>
2025-06-09 14:04:28 +0200

    Update src/vitabel/vitals.py
    Co-authored-by: Benjamin Hackl <devel@benjamin-hackl.at>

--- src/vitabel/vitals.py
+++ src/vitabel/vitals.py
@@ -1159,8 +1159,8 @@ class Vitals:
     def compute_etco2_and_ventilations(
         self,
         mode: Literal['threshold', 'filter'] = 'filter',
-        breath_thresh: float = 2,
-        etco2_thresh: float = 3,
+        breath_threshold: float = 2,
+        etco2_threshold: float = 3,
         **kwargs,
     ):
         """
