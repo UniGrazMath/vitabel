0594055845c5e2fe14b752e716ffa8a29aaafa4b
Simon Orlob <simon.orlob@medunigraz.at>
2025-06-12 14:06:03 +0200

    remove unnecessary styling

--- src/vitabel/utils/stylesheet.py
+++ src/vitabel/utils/stylesheet.py
@@ -223,16 +223,6 @@ DEFAULT_PLOT_STYLE = {
         "color": "firebrick",
         "alpha": 0.6,
     },
-    "cc_period_start_acc": { #Yet somehow unnecessary
-        "label": "CC Period Start (Accelerometry)",
-        "color": "forestgreen",        
-        "alpha": 0.6,
-    },
-    "cc_period_stop_acc": { #Yet somehow unnecessary
-        "label": "CC Period Stop (Accelerometry)",
-        "color": "firebrick",
-        "alpha": 0.6,
-    },
     "cc_periods" : {
         "label" : "CC Period",
         "color" : "lightsteelblue",
