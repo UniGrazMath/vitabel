2834a033e0bf9a117b8f5f8c2c14676b1c26e147
Simon Orlob <simon.orlob@medunigraz.at>
2025-06-24 15:46:38 +0200

    validate accelerometer channel existence before retrieval #217

--- src/vitabel/vitals.py
+++ src/vitabel/vitals.py
@@ -1841,7 +1841,11 @@ class Vitals:
             See https://unipub.uni-graz.at/obvugrhs/content/titleinfo/10138095 for more information.
         """
         if isinstance(accelerometer_channel, str):
-            ACC_channel = self.get_channel(accelerometer_channel)
+            if accelerometer_channel in self.get_channel_names():
+                ACC_channel = self.get_channel(accelerometer_channel)
+            else:
+                raise ValueError("The specified accelerometer channel ('{accelerometer_channel}') " \
+                "does not exist.")
         elif not isinstance(accelerometer_channel, Channel):
             raise ValueError(
                 "No valid accelerometer channel specified. Can not identify CC-periods via acceleration."
