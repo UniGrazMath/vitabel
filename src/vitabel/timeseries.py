5b2c4bdd650d5d4dc9f8944f0e67c9077519d68d
Benjamin Hackl <devel@benjamin-hackl.at>
2025-06-25 18:36:25 +0200

    cleanup implementation

--- src/vitabel/timeseries.py
+++ src/vitabel/timeseries.py
@@ -77,8 +77,10 @@ def _timeseries_list_info(series_list: list[TimeSeriesBase]) -> pd.DataFrame:
             )
 
     df = pd.DataFrame(info_dict).transpose()
-    df.columns = [col.title() for col in df.columns] #NOTE: debatable or if the should be written as callable, but see offset
-    desired_columns = ["Name", "Units", 'Length', 'First Entry', 'Last Entry', 'Offset', "Source", ]
+    df.columns = [col.title() for col in df.columns]  # capitalize column names
+    desired_columns = [
+        "Name", "Units", 'Length', 'First Entry', 'Last Entry', 'Offset', "Source"
+    ]
     df_columns = [col for col in desired_columns if col in df.columns]
     for col_name in ["Attached Channel", "Attached Labels"]:
         if col_name in df.columns:
